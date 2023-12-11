40ab79079d2e88c0174c7f4ed08ebd65a3fbe02f
Lee Jinsol <jins2ol@naver.com>
2023-12-12 01:33:33 +0900

    Feat - API functions for room management
    세부 정보 가져오기, 추가, 업데이트, 삭제 함수 추가

--- backend/controllers/roomControllers.ts
+++ backend/controllers/roomControllers.ts
@@ -1,7 +1,94 @@
 import { NextRequest, NextResponse } from "next/server";
+import Room from "../models/room";
 
 export const allRooms = async (req: NextRequest) => {
+  const resPerPage: number = 8;
+  const rooms = await Room.find();
   return NextResponse.json({
-    data: "hello",
+    success: true,
+    resPerPage,
+    rooms,
+  });
+};
+
+export const newRoom = async (req: NextRequest) => {
+  const body = await req.json();
+  const room = await Room.create(body);
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+export const getRoomDetails = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+export const updateRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  let room = await Room.findById(params.id);
+  const body = await req.json();
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  room = await Room.findByIdAndUpdate(params.id, body, {
+    new: true,
+  });
+
+  return NextResponse.json({
+    success: true,
+    room,
+  });
+};
+
+export const deleteRoom = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const room = await Room.findById(params.id);
+
+  if (!room) {
+    return NextResponse.json(
+      {
+        message: "Room not found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+
+  await room.deleteOne();
+
+  return NextResponse.json({
+    success: true,
   });
 };
---
+++ seeder/seeder.ts
@@ -0,0 +1,19 @@
+import Room from "../backend/models/room";
+import mongoose from "mongoose";
+import { rooms } from "./data";
+
+const seedRooms = async () => {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/bookit");
+    await Room.deleteMany();
+    console.log("Rooms are deleted");
+    await Room.insertMany(rooms);
+    console.log("Rooms are added");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit();
+  }
+};
+
+seedRooms();
